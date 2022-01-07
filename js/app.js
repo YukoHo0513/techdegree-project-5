00090796482da8c299f4ce89eb0e2d0af9056389
Ykhoo <89803794+YukoHo0513@users.noreply.github.com>
2022-01-07 15:43:55 -0800

    checked with validator

--- js/app.js
+++ js/app.js
@@ -1,3 +1,30 @@
 baguetteBox.run('.gallery', {
     captions : true,
 });
+
+
+const input = document.getElementById("search")
+const search = input.addEventListener("keyup", getValue);
+function getValue() {
+    const lCase = input.value.toLowerCase();
+    console.log(lCase);
+
+    let arr = document.getElementsByClassName("g-item")
+    for (let i = 0; i < arr.length; i++) {
+        const caption = arr[i].getAttribute('data-caption');
+        const lCaption = caption.toLowerCase();
+
+        let result = lCaption.includes(lCase);
+
+        let aTag = arr[i];
+        if (result) {
+            aTag.style.display = "block";
+        } else {
+            aTag.style.display = "none";
+        }
+    }
+}
+
+
+
+
